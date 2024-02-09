44aa060c25e85a85cde55673782dad7b819d850e
Mac <mac@192.168.0.100>
2024-02-09 18:49:57 +0530

    add chat api

---
+++ routes/chat.js
@@ -0,0 +1,74 @@
+const router=require("express").Router();
+var db = require('../db');
+const { io } = require("./socket");
+
+router.post('/create_chat', async (req,res)=>{
+    var status;
+    var message;
+    var sql="INSERT INTO chat (chat_participants) VALUES (?)";
+    db.query(sql,[req?.body?.chat_participants?.toString()] , function (err, rows) {
+        if (err) {
+            db.end();
+            message=err;
+            status="error";
+            res.status(200).json({status:status,message:message});
+        }
+        else
+        {
+            message="success";
+            status="success";
+            res.status(200).json({status:status,message:message});
+        }
+    });
+}), 
+
+router.post('/send_msg_api', async (req,res)=>{
+    var status;
+    var message;
+    var sql="INSERT INTO message (text, sender_id, chat_id) VALUES (?, ? , ?)";
+    db.query(sql,[req?.body?.text, req?.body?.sender_id, req?.body?.chat_id] , function (err, rows) {
+        if (err) {
+            db.end();
+            message=err;
+            status="error";
+            res.status(200).json({status:status,message:message});
+        }
+        else
+        {
+            const data = {
+                ...req?.body,
+                id: rows?.insertId,
+                created_at: new Date().toISOString(),
+                updated_at: new Date().toISOString()
+            }
+            io.to(req.body?.chat_id).emit("chat_message", data);
+            message="success";
+            status="success";
+            res.status(200).json({status:status,message:message, data: data});
+        }
+    });
+}), 
+
+router.post('/get_chat_msg', async (req,res)=>{
+    var status;
+    var message;
+    var sql="SELECT * FROM message WHERE chat_id = ?";
+    db.query(sql,[req?.body?.chat_id] , function (err, rows) {
+        if (err) {
+            db.end();
+            message=err;
+            status="error";
+            res.status(200).json({status:status,message:message});
+        }
+        else
+        {
+            message="success";
+            status="success";
+            res.status(200).json({status:status,message:message, data : rows});
+        }
+    });
+}), 
+
+
+
+module.exports=router 
