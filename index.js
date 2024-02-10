8084b161c3462a0c8836e65818ccc0441c85adca
Mac <mac@192.168.0.101>
2024-02-10 19:03:33 +0530

    fixed bug

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
-
+require('./routes/socket.js')
 dotenv.config()
 
 app.use(express.json());
@@ -18,13 +18,14 @@ const Matches = require('./routes/matches');
 const Industry = require('./routes/industry');
 
 const Sendmail = require('./routes/sendemail');
-
+const chat = require('./routes/chat.js');
 
 app.use('/api/Users', userRout)
 app.use('/api/Login', Login)
 app.use('/api/Matches', Matches)
 app.use('/api/industry', Industry)
 app.use('/api/Sendmail', Sendmail)
+app.use('/api/chat', chat)
 
 app.listen(5001, () => {
     console.log("application is running");
