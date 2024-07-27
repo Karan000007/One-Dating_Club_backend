c03164e938911be8af547d3ea9facdf3548f6a10
megavth venkatesh <144617554+megavathvenkatesh@users.noreply.github.com>
2024-07-27 09:41:31 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,15 +1,37 @@
+// var mysql = require('mysql');
+// var connection = mysql.createConnection({
+//     host     : 'localhost',
+//     user     : 'root',
+//     password : '',
+//     database : 'onedatingclub',
+//     charset : 'utf8mb4'
+// });
+
+// connection.connect(function(err) {
+//     if (err) throw err;
+//     console.log("Connected!");
+// });
+
+// module.exports = connection;
+
+require('dotenv').config();
 var mysql = require('mysql');
+
 var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : '',
-    database : 'onedatingclub',
-    charset : 'utf8mb4'
+    host     : process.env.DB_HOST,
+    port     : process.env.DB_PORT,
+    user     : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_NAME,
+    charset  : 'utf8mb4'
 });
 
 connection.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
+    if (err) {
+        console.error('Error connecting to MySQL:', err);
+        throw err;
+    }
+    console.log("Connected to MySQL");
 });
 
 module.exports = connection;
